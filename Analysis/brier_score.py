43a8bfdb96e8560caa157ea40e355f30b209c0cb
damienld <dledirach@yahoo.fr>
2022-01-01 10:51:07 +0100

    Update brier_score.py

--- Analysis/brier_score.py
+++ Analysis/brier_score.py
@@ -14,7 +14,17 @@ def apply_brier(col_result: int, col_prediction: float):
         case _:
             return 0 
     
-def calc_brier(df: DataFrame, col_result: str, col_prediction: str)->DataFrame:
-    
-    df["brier"] = df.apply(lambda row: apply_brier(row[col_result], row[col_prediction]), axis=1)
+def calc_brier(df: DataFrame, col_result: str, col_prediction: str, newcolname: str="brier")->DataFrame:
+    """[summary]
+
+    Args:
+        df (DataFrame): Will be used to get columns data and will be returned with the new column @newcolname
+        col_result (str): Name of the column with the result value (format int: 0 or 1 (player 1 or player 2 won))
+        col_prediction (str): Name of the columns with the probability to be tested
+        newcolname (str, optional): Name fo the columns to input the result. Defaults to "brier".
+
+    Returns:
+        DataFrame: with the new column
+    """
+    df[newcolname] = df.apply(lambda row: apply_brier(row[col_result], row[col_prediction]), axis=1)
     return df
