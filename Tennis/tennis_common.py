112521a79cb995b3d47e05548fa6e964f1f2e115
damienld <dledirach@yahoo.fr>
2022-01-01 10:51:32 +0100

    Create tennis_common.py

---
+++ Tennis/tennis_common.py
@@ -0,0 +1,11 @@
+import datetime
+
+DateStartOffSeason = (1, 12)
+DateEndOffSeason = (25, 12)
+
+
+def calc_datediff_withoutoffseason(date2: datetime, date1: datetime) -> int:
+    if date2 > datetime(olddate.year, 11, 1) and olddate < datetime(
+        olddate.year, 12, 25
+    ):
+        date - datetime(year, 11, 1)
